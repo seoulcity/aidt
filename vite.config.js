5f18a7c3ff60e012211ae84e78b28c38bc23465e
Kim Jung Hyun <lallacri86@gmail.com>
2025-04-18 18:20:36 +0900

    Vercel

--- vite.config.js
+++ vite.config.js
@@ -5,12 +5,20 @@ export default defineConfig({
 	plugins: [sveltekit()],
 	optimizeDeps: {
 		include: ['pdfjs-dist'],
-		exclude: ['pdfjs-dist/build/pdf.worker.min.js']
+		exclude: ['pdfjs-dist/build/pdf.worker.min.js', 'langchain', 'langsmith', 'p-queue']
 	},
 	build: {
 		commonjsOptions: {
 			include: [/pdfjs-dist/]
-		}
+		},
+		rollupOptions: {
+			// langchain 관련 외부 모듈로 처리
+			external: ['langchain', 'langsmith', 'p-queue'],
+			output: {
+				format: 'es'
+			}
+		},
+		ssr: false
 	},
 	server: {
 		fs: {
