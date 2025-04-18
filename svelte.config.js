bc258c57d663a17f5815aa068b1e4e0725eca7be
Kim Jung Hyun <lallacri86@gmail.com>
2025-04-18 18:17:19 +0900

    Vercel

--- svelte.config.js
+++ svelte.config.js
@@ -1,10 +1,13 @@
-import adapter from '@sveltejs/adapter-auto';
+import adapter from '@sveltejs/adapter-vercel';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	kit: {
-		adapter: adapter(),
+		adapter: adapter({
+			// Vercel Edge 함수 대신 Node.js 함수 사용
+			runtime: 'nodejs18.x'
+		}),
 	},
 	preprocess: vitePreprocess()
 };
